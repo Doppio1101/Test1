4a8b5ef7567ee928e98e2b0f1d7444dd64b9f024
JungHun Jang <chlj1101@gmail.com>
2021-04-11 15:13:59 +0900

    atom js file

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+function board(){
+  var boardnum = document.querySelector(".num").value;
+  alert(boardnum[1]);
+}
+function goboard(){
+  window.open("board.html","_self","");
+}
+function gomain(){
+  window.open("main.html","_self","");
+}
+function goorder(){
+  window.open("order.html","_self","");
+}
